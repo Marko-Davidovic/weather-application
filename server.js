49b5547bcb14ea080d4f6b28b3b2a6d244522ba7
Marko Davidovic <davidovic-m@hotmail.com>
2020-05-14 21:49:22 -0400

    second commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+
+// Initiate server
+const server = http.createServer(app);
+
+// Ensure https://
+if(process.env.NODE_ENV === 'production' && process.env.SSL_NO_REDIRECT !== 'true') {
+  const sslRedirect = require('heroku-ssl-redirect');
+  app.use(sslRedirect());
+}
+
+// Serve assets
+app.use('/', express.static(__dirname + '/build'));
+
+app.use((req, res) => {
+  // 404
+  res.status(404).send('Not Found');
+});
+
+// Start listening on port 3000 or provided by server
+server.listen(process.env.PORT || 3000, () => {
+  console.log('App is listening at %s on port %s',
+    server.address().address,
+    server.address().port);
+});
