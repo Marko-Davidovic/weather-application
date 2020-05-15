4a0e3d693eba6e65169c9b51f89d2ad4264ff712
Marko Davidovic <davidovic-m@hotmail.com>
2020-05-14 23:18:37 -0400

    fix-1

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
         <div>
           <div className="location-box">
             <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
+            <div className="date">{dateBuilder(new Date().toString())}</div>
           </div>
           <div className="weather-box">
             <div className="temp">
